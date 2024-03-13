ceb024d09a526303711e296aa15281814267e7de
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2024-03-13 23:08:33 +0530

    Update [noteid].js

--- src/pages/newnotes/[noteid].js
+++ src/pages/newnotes/[noteid].js
@@ -138,7 +138,6 @@ export default function Note() {
               }} className="block px-2 text-4xl placeholder:text-white border-0 focus:border-none text-center text-white bg-black min-w-72 h-fit min-h-16 w-fit" type="text" ></input>
               <textarea value={content} onChange={(e) => {
                 setContent(e.target.value)
-                  socket?.emit('update content', e.target.value)
                 const value = e.target.value; if (value[value.length - 1] === ' ' || value[value.length - 1] === '.') {
                   socket?.emit('update content', e.target.value)
                   savedata()
