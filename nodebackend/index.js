2bae6741f12883a34f62bf49663966f9d0e17fb7
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2024-03-15 16:08:59 +0530

    Update index.js

--- nodebackend/index.js
+++ nodebackend/index.js
@@ -1,47 +1,32 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import cors from "cors"
+import cors from 'cors';
 import dotenv from 'dotenv';
+
 dotenv.config({ path: '.env.local' });
+
 const app = express();
-app.use(cors())
 const httpServer = createServer(app);
-const io = new Server(httpServer, {cors: {
-  origin: process.env.ORIGIN, 
-  methods: ["GET", "POST"] 
-}});
+const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(cors());
 
 io.on('connection', (socket) => {
   socket.on('update content', (content) => {
     console.log(content);
-    io.emit('content update', content);
+    socket.broadcast.emit('content update', content); // Broadcast to all clients except the sender
   });
+
   socket.on('update title', (content) => {
     console.log(content);
-    io.emit('title update', content);
+    socket.broadcast.emit('title update', content); // Broadcast to all clients except the sender
   });
-});
-
-app.get('/socket', (req, res) => {
-  if (res.socket.server.io) {
-    console.log('Socket is already running');
-  } 
-  else {
-    console.log('Socket is initializing');
-    const io = new Server(res.socket.server,{cors: {
-      origin: process.env.ORIGIN, 
-      methods: ["GET", "POST"] 
-    }});
-    res.socket.server.io = io;
-    io.on('connection', (socket) => {
-      socket.on('update content', (content) => {
-        console.log(content);
-        io.emit('content update', content);
-      });
-    });
-  }
-  res.end();
 });
 
 const PORT = process.env.PORT || 5000;
